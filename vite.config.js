fea7873f3bf67bbffb0d805a3c16b7b2c074e760
MahmoudND <mahmoudnasseraldine836@gmail.com>
2026-05-14 11:58:59 +0300

    Fix: add base path to vite config and rebuild dist

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // أضف هذا السطر لجعل جميع مسارات الملفات نسبية
 })
